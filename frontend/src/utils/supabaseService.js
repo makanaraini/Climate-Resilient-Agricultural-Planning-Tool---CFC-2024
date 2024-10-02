46f41ce453dd0e81737defe4faa8f6e92eec5599
makanaraini <danielraini871@gmail.com>
2024-10-02 15:38:07 +0300

    deleted supabaseClient

--- frontend/src/utils/supabaseService.js
+++ frontend/src/utils/supabaseService.js
@@ -2,13 +2,20 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL and Key must be defined in the environment variables.');
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const fetchWeatherData = async () => {
-    const { data, error } = await supabase
-        .from('weather_data')
-        .select('*');
-    if (error) throw new Error(error.message);
-    return data;
+  const { data, error } = await supabase
+    .from('weather_data')
+    .select('*');
+  if (error) throw new Error(error.message);
+  return data;
 };
+
+export { supabase };
